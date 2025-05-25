b76fa1adaebd895e3d74474d61f20d941a1aaea2
John <jmtaylor.dev@gmail.com>
2025-05-25 20:37:56 +1000

    - fix typo

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Homepage from './pages/Homepage.jsx'
 import Resume from './pages/Resume.jsx'
 import { Toaster } from 'react-hot-toast';
-import './app.css'
+import './App.css'
 
 export default function App() {
   return (
